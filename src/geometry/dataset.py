d575576821854a86dcbe3146f16a545036c83243
Artem Liakhov <Liakhov.Artem@yandex.ru>
2026-06-29 14:25:28 +0300

    Update dataset module * Add support of reading datasets with any labels * Update docstrings

--- src/geometry/dataset.py
+++ src/geometry/dataset.py
@@ -18,6 +18,18 @@ class ImageAnnotation:
 
 
 def read_image(annotation_fname, image_id):
+    """
+    Прочитать изображение
+
+    Args:
+        annotation_fname (str): путь к файлу с JSON-файлу с разметкой
+            Файл с изображением должен лежать в папке images в той же 
+            директории, что и JSON-файл
+        image_id (str): идентификатор изображения
+
+    Returns:
+        np.ndarray: изображение
+    """
     file_path = Path(annotation_fname)
     file_dir = file_path.parent
 
@@ -31,31 +43,55 @@ def read_image(annotation_fname, image_id):
 
 
 def read_json(fname, *args, **kwargs):
+    """
+    Прочитать JSON-файл
+    """
     with open(fname, encoding="utf-8") as file:
         return json.load(file, *args, **kwargs)
 
 
 def save_json(data, fname, *args, **kwargs):
+    """
+    Сохранить данные в JSON-файл
+    """
     with open(fname, "w", encoding="utf-8") as file:
         return json.dump(data, file, *args, **kwargs)
 
 
-def load_dataset(fname):
+def load_dataset(fname, label_name_mapping=None):
     """
-    Прочитать датасет
+    Загрузить данные
+
+    Args:
+        fname (str): путь к файлу с JSON-файлу с разметкой.
+            Предполагается, что изображения лежат в папке images в той
+            же директории, что и JSON-файл
+        label_name_mapping (dict[str, str]): словарь для переименования классов
+            (по умолчанию None)
+    
+    Returns
+        list[ImageAnnotation]: список объектов ImageAnnotation
+
     """
+    if label_name_mapping is None:
+        label_name_mapping = {}
+
     annotation = read_json(fname)
     result = []
 
     for image_id, image_annotation in annotation.items():
-        ellipses = [
-            Ellipse(label="data", **obj)
-            for obj in image_annotation["Эритроцит"]
-        ]
+        ellipses = []
+        
+        for class_name, objects in image_annotation.items():
+            label_name = label_name_mapping.get(class_name, class_name)
+
+            ellipses.extend([
+                Ellipse(label=label_name, **obj)
+                for obj in objects
+            ])
         
         result.append(
             ImageAnnotation(
                 image_id=image_id, ellipses=ellipses, image=read_image(fname, image_id)
             ))
     return result
-
